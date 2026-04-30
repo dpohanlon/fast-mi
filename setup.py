47a1dcb366bcf8f8656ab6e7d1368776fbfe6c39
Daniel O'Hanlon <dpohanlon@gmail.com>
2026-04-30 15:05:20 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name="mimiq",
-    version="0.1.1",
+    version="0.2.0",
     description="Fast pairwise mutual information computation.",
     author="Daniel O'Hanlon, Sergio Garcia-Busto",
     author_email="dpohanlon@gmail.com",
